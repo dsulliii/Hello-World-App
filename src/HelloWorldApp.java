33539dd31ad1427fa85c477e34d6c4e7738b22e9
David Sullivan <dsulliiI@aol.com>
2019-09-15 17:54:15 -0600

    Changed 2nd line

--- src/HelloWorldApp.java
+++ src/HelloWorldApp.java
@@ -1,6 +1,6 @@
 public class HelloWorldApp {
 	public static void main(String[] args) {
 		 System.out.println("Hello World!");
-		 System.out.println("Hello to you, too, my friend.");
+		 System.out.println("Hello to you, too.");
 	}
 } 
